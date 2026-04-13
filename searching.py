5b692c10282da374c62fbb93f88c0b023b502461
Jakub Michalík <jmmichali2@gmail.com>
2026-04-13 13:27:21 +0200

    commit3

--- searching.py
+++ searching.py
@@ -2,14 +2,24 @@ from pathlib import Path
 import json
 
 def read_data(file_name, field):
-    file_path = Path.cwd() / file_name
+    file_cesta = Path.cwd() / file_name
 
-    if not file_path.exists():
+    if not file_cesta.exists():
         return None
-    with open(file_path, "r", encoding="utf=8") as f:
-        data = json.load(f)
+    with open(file_cesta, "r", encoding="utf=8") as subor:
+        data = json.load(subor)
     return data.get(field,None)
 
+def main():
+    unordered = read_data("data.json", "unordered_numbers")
+    ordered = read_data("data.json", "ordered_numbers")
+    dna = read_data("data.json", "dna_sequence")
 
+    print("Unordered numbers:", unordered[:10] if unordered else None)
+    print("Ordered numbers:", ordered[:10] if ordered else None)
+    print("DNA:", dna[:50] if dna else None)
+
+if __name__ == ("__main__"):
+    main()
 
 
